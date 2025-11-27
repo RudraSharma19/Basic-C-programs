43909825d62935fcdbeb1b952b838cd794316d84
Rudra Sharma <rudrasharma2219@gmail.com>
2025-11-27 14:25:04 +0530

    Delete 100days2.c

--- 100days2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	int a,b;
-	printf("Enter two numbers\n");
-	scanf("%d",&a);
-	scanf("%d",&b);
-	printf("Sum = %d, Difference = %d, Product = %d, Quotient = %d",a+b,a-b,a*b,a/b);
-}

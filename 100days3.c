716a0ca87288ae447b03a0438105742ada9c4434
Rudra Sharma <rudrasharma2219@gmail.com>
2025-11-27 14:27:00 +0530

    Delete 100days3.c

--- 100days3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	int a,b;
-	printf("Enter teo sides of a Rectangle\n");
-	scanf("%d",&a);
-	scanf("%d",&b);
-	printf("The Area = %d, Perimeter = %d\n",a*b,2*(a+b));
-}
